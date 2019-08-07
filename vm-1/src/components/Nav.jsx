fcb36f7eedab4b2146b9e84db41a44c5b23cd14b
Bradley Windybank <trykryda@gmail.com>
2019-08-07 15:59:14 +1200

    Delete all confirmation added. Closes #9

--- vm-1/src/components/Nav.jsx
+++ vm-1/src/components/Nav.jsx
@@ -17,7 +17,7 @@ export default class Nav extends React.Component {
       note: this.state.note
     };
 
-    axios.post(`http://192.168.55.11:3000/note`, { note }).then(res => {
+    axios.post(`http://localhost:3000/note`, { note }).then(res => {
       console.log(res);
       console.log(res.data);
     });
@@ -28,7 +28,7 @@ export default class Nav extends React.Component {
   deleteFunction = event => {
     event.preventDefault();
 
-    axios.delete(`http://192.168.55.11:3000/deleteallnotes`, {}).then(res => {
+    axios.delete(`http://localhost:3000/deleteallnotes`, {}).then(res => {
       console.log(res);
       console.log(res.data);
     });
@@ -54,10 +54,17 @@ export default class Nav extends React.Component {
             <button className="btn btn-outline-success" type="submit">
               Add
             </button>
+
             <button
               className="btn btn-outline-danger ml-2"
-              onClick={this.deleteFunction}
-              type="button"
+              onClick={event => {
+                event.preventDefault();
+
+                if (
+                  window.confirm("Are you sure you wish to delete ALL tasks?")
+                )
+                  this.deleteFunction(event);
+              }}
             >
               Delete All
             </button>
